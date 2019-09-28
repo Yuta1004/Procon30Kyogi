ee0a4d4f28f620f97eaa3106c6960fc3e6ea1c0c
NakagamiYuta <guguru0014@gmail.com>
2019-09-28 23:42:02 +0900

    [add] GetConfigData作成
    テストOK

--- config/reader.go
+++ config/reader.go
@@ -1,5 +1,16 @@
 package config
 
+import (
+	"github.com/BurntSushi/toml"
+	"os"
+	"path"
+)
+
+var (
+	config   Config
+	rootPath = path.Join(os.Getenv("GOPATH"), "src/github.com/Yuta1004/procon30-kyogi")
+)
+
 // Config : 設定情報を扱う構造体
 type Config struct {
 	GameServer GameServer
@@ -8,4 +19,15 @@ type Config struct {
 // GameServer : 設定情報(GameServer)を扱う構造体
 type GameServer struct {
 	URL string
+}
+
+// GetConfigData : 設定情報を返す
+func GetConfigData() *Config {
+	if config.GameServer.URL == "" {
+		_, err := toml.DecodeFile(rootPath+"/config.toml", &config)
+		if err != nil {
+			return nil
+		}
+	}
+	return &config
 }
