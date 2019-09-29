d2b8855f5d84c62cb6bd5f12057b80961de7345d
NakagamiYuta <guguru0014@gmail.com>
2019-09-29 15:49:55 +0900

    [add] ExecSolver内でソルバに渡すJSONの生成をするようにした

--- manager/solver/solvermanager.go
+++ manager/solver/solvermanager.go
@@ -1,10 +1,20 @@
 package solver
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/Yuta1004/procon30-kyogi/manager/battle"
+	"os"
 )
 
 // ExecSolver : ソルバを起動, 実行する
 func ExecSolver(ch chan string, battle battle.Battle) {
-
+	// setting json
+	jsonStr, err := json.Marshal(battle.DetailInfo)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not convert to json from \"battle\" : %s\n", err)
+		ch <- "Error"
+		return
+	}
+	_ = jsonStr
 }
