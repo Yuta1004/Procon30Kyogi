567a33e304679d62f01e7adf02bc70ec1a08a90a
NakagamiYuta <guguru0014@gmail.com>
2019-10-04 00:38:10 +0900

    [add] main.goでCUIを呼ぶようにした

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Yuta1004/procon30-kyogi/config"
+	"github.com/Yuta1004/procon30-kyogi/cui"
 	"github.com/Yuta1004/procon30-kyogi/manager/battle"
 	"log"
 )
 
 func main() {
+	// setting fot mylog
+	for cnt := 0; cnt < 6; cnt++ {
+		fmt.Println()
+	}
+
+	// exec CUI
+	go cui.CUI()
+
+	// exec battle manager
 	conf := config.GetConfigData()
 	for {
 		battle.BManagerExec(conf.GameServer.Token)
