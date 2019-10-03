98b6540c5bb225bd41848d42e453bbbee79caf30
NakagamiYuta <guguru0014@gmail.com>
2019-10-03 15:27:47 +0900

    [fix] mylog内でのPrintf指定にミスがあったので修正

--- mylog/mylog.go
+++ mylog/mylog.go
@@ -8,30 +8,34 @@ import (
 func Info(fmt string, args ...interface{}) {
 	header := "[INFO] "
 	footer := "\n"
-	outlog(header, fmt, footer, args)
+	outlog(header, fmt, footer, args...)
 }
 
 // Error : タイプErrorのログを出力する
 func Error(fmt string, args ...interface{}) {
 	header := "\x1b[31m[ERROR] "
 	footer := "\x1b[0m\n"
-	outlog(header, fmt, footer, args)
+	outlog(header, fmt, footer, args...)
 }
 
 // Notify : タイプNotifyのログを出力する
 func Notify(fmt string, args ...interface{}) {
 	header := "\x1b[32m[NOTIFY] "
 	footer := "\x1b[0m\n"
-	outlog(header, fmt, footer, args)
+	outlog(header, fmt, footer, args...)
 }
 
 // Warning : タイプWarningのログを表示する
 func Warning(fmt string, args ...interface{}) {
 	header := "\x1b[33m[WARNING] "
 	footer := "\x1b[0m\n"
-	outlog(header, fmt, footer, args)
+	outlog(header, fmt, footer, args...)
 }
 
 func outlog(header, fmt, footer string, args ...interface{}) {
-	log.Printf(fmt, args)
+	if len(args) > 0 {
+		log.Printf(header+fmt+footer, args...)
+	} else {
+		log.Printf(header + fmt + footer)
+	}
 }
