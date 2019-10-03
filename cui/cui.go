343c9c51446f65398708b1102a254f539698dcec
NakagamiYuta <guguru0014@gmail.com>
2019-10-04 01:23:06 +0900

    [add] exitコマンド実装

--- cui/cui.go
+++ cui/cui.go
@@ -20,8 +20,24 @@ func CUI() {
 
 	// mainloop
 	for {
-		<-ch
+		// input
+		inp := <-ch
 		mylog.SetInputArea(string(inpBuf))
+		if inp != byte('\n') {
+			continue
+		}
+
+		// command
+		mylog.Warning("-%s-", string(inpBuf))
+		switch string(inpBuf) {
+		case "exit":
+			mylog.Info("システムを終了します...")
+			os.Exit(0)
+		}
+
+		// clean buf
+		inpBuf = nil
+		inpBuf = make([]byte, 1024)
 	}
 }
 
@@ -36,6 +52,8 @@ func monitorStdin(ch chan byte) {
 			os.Exit(0)
 		case keyboard.KeyBackspace, keyboard.KeyBackspace2:
 			inpBuf = inpBuf[:len(inpBuf)-1]
+		case keyboard.KeyEnter:
+			char = '\n'
 		default:
 			inpBuf = append(inpBuf, byte(char))
 		}
