d451012295fbe0f6a6e85a4dffc306d8f23ab470
NakagamiYuta <guguru0014@gmail.com>
2019-10-05 13:43:32 +0900

    [add] 設定ファイル読み込みエラーの際にログを吐くようにした

--- config/reader.go
+++ config/reader.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/BurntSushi/toml"
+	"github.com/Yuta1004/procon30-kyogi/mylog"
 )
 
 var (
@@ -30,6 +31,8 @@ func GetConfigData() *Config {
 	if config.GameServer.URL == "" {
 		_, err := toml.DecodeFile("config.toml", &config)
 		if err != nil {
+			mylog.Error("設定ファイルの読み込み中にエラーが発生しました")
+			mylog.Error(err.Error())
 			return nil
 		}
 	}
