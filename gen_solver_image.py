30a787454d4b98e8629f98b99af6e990739cff23
NakagamiYuta <guguru0014@gmail.com>
2019-10-07 00:06:48 +0900

    [add] gen_solver_image.pyにソルバプログラムを探索する処理を追加

--- gen_solver_image.py
+++ gen_solver_image.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 
 def make_base_image():
@@ -10,12 +11,13 @@ def make_solver_image(solver_path):
 
 
 def get_solver_list():
-    return []
+    return glob.glob("./solvers/*/solver_*.py")
 
 
 def main():
     print("Step1 : ソルバプログラムを探しています...")
     solver_list = get_solver_list()
+    print("  -> " + str(len(solver_list)) + "つのソルバプログラムが見つかりました\n")
 
     print("Step2 : ベースイメージを生成しています...")
     make_base_image()
