078360dcc8911cbedd32518d8a31edc03983b54a
NakagamiYuta <guguru0014@gmail.com>
2019-10-07 00:03:24 +0900

    [add] gen_solver_image.py

---
+++ gen_solver_image.py
@@ -0,0 +1,29 @@
+import os
+
+
+def make_base_image():
+    pass
+
+
+def make_solver_image(solver_path):
+    pass
+
+
+def get_solver_list():
+    return []
+
+
+def main():
+    print("Step1 : ソルバプログラムを探しています...")
+    solver_list = get_solver_list()
+
+    print("Step2 : ベースイメージを生成しています...")
+    make_base_image()
+
+    print("Step3 : ソルバイメージを作成しています...")
+    for solver_path in solver_list:
+        make_solver_image(solver_path)
+
+
+if __name__ == "__main__":
+    main()
