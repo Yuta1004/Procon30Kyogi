eb8a5fc10f0dfaf34534eae6087d080e06be4666
NakagamiYuta <guguru0014@gmail.com>
2019-10-08 21:24:06 +0900

    [add] configコマンド追加

--- cui/cui.go
+++ cui/cui.go
@@ -65,6 +65,14 @@ func execCommand(command ...string) {
 		config.SetConfigData(*conf)
 		mylog.Notify("使用するトークンを変更しました -> %s", command[1])
 
+	case "config":
+		config := config.GetConfigData()
+		mylog.Notify("\x1b[1m----- 現在の設定状況 -----")
+		mylog.Notify("ゲームサーバURL: %s", config.GameServer.URL)
+		mylog.Notify("トークン: %s", config.GameServer.Token)
+		mylog.Notify("ソルバイメージ: %s", config.Solver.Image)
+		mylog.Notify("\x1b[1m------------------------")
+
 	case "refresh":
 		mylog.Warning("試合情報を再取得します...(更新終了まで操作をしないでください)")
 		conf := config.GetConfigData()
