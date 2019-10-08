23b168a3090e35f99c78be310aa758afdaf06f76
NakagamiYuta <guguru0014@gmail.com>
2019-10-08 22:27:40 +0900

    [change] connectorのテストを大幅に書き直した(GetAllBattleのみ)

--- connector/gameserver_test.go
+++ connector/gameserver_test.go
@@ -1,19 +1,65 @@
 package connector
 
 import (
+	"github.com/Yuta1004/procon30-kyogi/config"
 	"os"
+	"os/exec"
 	"path"
 	"testing"
+	"time"
 )
 
 var (
 	rootPath = path.Join(os.Getenv("GOPATH"), "src/github.com/Yuta1004/procon30-kyogi")
 )
 
-func TestGetAllBattle(t *testing.T) {
+func TestConnector(t *testing.T) {
+	// setting (config)
 	os.Chdir(rootPath)
-	battleInfo := GetAllBattle("A")
-	if battleInfo == nil {
-		t.Fail()
+	token := "procon30_example_token"
+	config.SetConfigData(config.Config{
+		GameServer: config.GameServer{URL: "http://localhost:8081", Token: token},
+	})
+
+	// setting(server)
+	cmd := exec.Command("./procon-server_darwin", "--port=8081")
+	cmd.Dir = rootPath
+	cmd.Start()
+	time.Sleep(time.Second)
+	defer func() {
+		cmd.Process.Kill()
+	}()
+
+	// do test
+	testGetAllBattle(t, token)
+}
+
+// GetAllBattleのテスト
+func testGetAllBattle(t *testing.T, token string) {
+	os.Chdir(rootPath)
+	battleInfo := *GetAllBattle(token)
+
+	// idx 0
+	battle := battleInfo[0]
+	if battle.ID != 1 {
+		t.Fatal("Error: testAllBattle001")
+	}
+	if battle.MatchTo != "A高専" {
+		t.Fatal("Error: testAllBattle002")
+	}
+
+	// idx 1
+	battle = battleInfo[1]
+	if battle.IntervalMillis != 5000 {
+		t.Fatal("Error: testAllBAttle003")
+	}
+	if battle.TurnMillis != 20000 {
+		t.Fatal("Error: testAllBattle004")
+	}
+
+	// idx 2
+	battle = battleInfo[2]
+	if battle.MaxTurn != 60 {
+		t.Fatal("Error : testAllBattle005")
 	}
 }
