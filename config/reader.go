ba04cce9f292ce588d2c28cc10a6f7f76db0f621
NakagamiYuta <guguru0014@gmail.com>
2019-10-11 21:40:05 +0900

    [add] Solver構造体にGet/Setレシーバ関数を追加した
    試合ごとに使うソルバイメージを変更できるように

--- config/reader.go
+++ config/reader.go
@@ -23,13 +23,15 @@ type GameServer struct {
 
 // Solver : 設定情報(Solver)を扱う構造体
 type Solver struct {
-	Image string
+	Image     string         // デフォルト(ファイルから読む)
+	manualSet map[int]string // 試合IDごとにもつ
 }
 
 // GetConfigData : 設定情報を返す
 func GetConfigData() *Config {
 	if config.GameServer.URL == "" {
 		_, err := toml.DecodeFile("config.toml", &config)
+		config.Solver.manualSet = make(map[int]string)
 		if err != nil {
 			mylog.Error("設定ファイルの読み込み中にエラーが発生しました")
 			mylog.Error(err.Error())
@@ -42,4 +44,21 @@ func GetConfigData() *Config {
 // SetConfigData : 設定情報を新しくセットする
 func SetConfigData(conf Config) {
 	config = conf
+	if config.Solver.manualSet == nil {
+		config.Solver.manualSet = make(map[int]string)
+	}
+}
+
+// Get : 任意の試合IDで使うソルバイメージを参照する
+func (s Solver) Get(battleID int) string {
+	val, exists := s.manualSet[battleID]
+	if exists {
+		return val
+	}
+	return s.Image
+}
+
+// Set : 任意の試合IDで使用するソルバイメージを変更する
+func (s Solver) Set(battleID int, image string) {
+	s.manualSet[battleID] = image
 }
