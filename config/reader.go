f14f580bbb20904ceeffd38628fa7f2dd2b45c14
NakagamiYuta <guguru0014@gmail.com>
2019-10-11 21:41:17 +0900

    [change] Config関連の構造体定義を別ファイルで行うようにした

--- config/reader.go
+++ config/reader.go
@@ -9,24 +9,6 @@ var (
 	config Config
 )
 
-// Config : 設定情報を扱う構造体
-type Config struct {
-	GameServer GameServer
-	Solver     Solver
-}
-
-// GameServer : 設定情報(GameServer)を扱う構造体
-type GameServer struct {
-	URL   string
-	Token string
-}
-
-// Solver : 設定情報(Solver)を扱う構造体
-type Solver struct {
-	Image     string         // デフォルト(ファイルから読む)
-	manualSet map[int]string // 試合IDごとにもつ
-}
-
 // GetConfigData : 設定情報を返す
 func GetConfigData() *Config {
 	if config.GameServer.URL == "" {
