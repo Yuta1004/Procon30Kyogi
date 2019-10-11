1c347025a218cb6bfef33d8b2c1bc3f9dd1d15a1
NakagamiYuta <guguru0014@gmail.com>
2019-10-11 21:51:56 +0900

    [change] Solver(config)構造体のレシーバ関数を専用ファイルに移した

--- config/reader.go
+++ config/reader.go
@@ -30,17 +30,3 @@ func SetConfigData(conf Config) {
 		config.Solver.manualSet = make(map[int]string)
 	}
 }
-
-// Get : 任意の試合IDで使うソルバイメージを参照する
-func (s Solver) Get(battleID int) string {
-	val, exists := s.manualSet[battleID]
-	if exists {
-		return val
-	}
-	return s.Image
-}
-
-// Set : 任意の試合IDで使用するソルバイメージを変更する
-func (s Solver) Set(battleID int, image string) {
-	s.manualSet[battleID] = image
-}
